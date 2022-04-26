f0f60daf03c2b7d51b85f69c2b8996afd777ad24
kthiramitsu <kthiramitsu@nissho-ele.co.jp>
2022-04-26 15:26:24 +0900

    twelveth

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ terraform {
        version = "=2.46.0"
       }
     }
+
+    backend "azurerm" {
+        resource_group_name = "rg-for-storage"
+        storage_account_name = "koehatesutodesuka"
+        container_name = "tfstate"
+        key = "terraform.tfstate"
+        access_key = "x1zOX3Fdm5td3tkSa+f7N4eNOFEr5BZ+9Bgyts8Ioy08rOHda9PD5+Murbdhqiw5PEZqPVTgcH0M8GRdJmVM7w=="
+      
+    }
 }
 
 provider "azurerm" {
